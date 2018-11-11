079e35a9adf6fe746b03d8ddbcdfccf5713dabee
Dhiran08 <dhiranandi08@gmail.com>
2018-11-11 13:32:05 +0700

    buat bintang sederhana coba

--- Coba_coba.js
+++ Coba_coba.js
@@ -1,3 +1,13 @@
 console.log('Hallo');
 
 var Salam = 'Halo Semua ^_^';
+
+var z = ''
+for (let i = 0; i <= 5; i++) {
+    for (let j = 0; j <= 5; j++) {
+        z += '*';
+
+    }
+    z += '\n';
+}
+console.log(z);
